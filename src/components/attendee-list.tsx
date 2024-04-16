f6e54eb06e051e7a8f919ea3cbe5e88a30b83639
Arthur VR <arthurolov@gmail.com>
2024-04-16 12:19:57 -0300

    feat: add @tailwind/forms for checkbox customization and finished table customization with array for testing compatibility

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -1,4 +1,4 @@
-import { Search } from 'lucide-react'
+import { ChevronLeft, ChevronRight, ChevronsLeft, ChevronsRight, MoreHorizontal, Search } from 'lucide-react'
 
 export function AttendeeList() {
     return (
@@ -7,30 +7,32 @@ export function AttendeeList() {
                 <h1 className="text-2xl font-bold">Participantes</h1>
                 <div className="px-3 w-72 py-1.5 border border-white/10 rounded-lg text-sm flex items-center gap-3">
                     <Search className='size-4 text-emerald-300'/>  {/*o mesmo que w-4 h-4*/} 
-                    <input className="bg-transparent flex-1 outline-none" placeholder="Buscar participantes ..." />
+                    <input className="bg-transparent flex-1 outline-none border-0 p-0 text-sm" placeholder="Buscar participantes ..." />
                 </div>
             </div>
 
             <div className='border border-white/10 rounded-lg'>
                 <table className='w-full'>
                     <thead>
-                        <tr className='border-b border-white/10'>
-                            <th className='py-3 px-4 text-sm font-semibold text-left'>
-                                <input type="checkbox" />
+                        <tr className='border-b border-white/1'>
+                            <th style={{ width: 48 }} className='py-3 px-4 text-sm font-semibold text-left'>
+                                <input type="checkbox" className='size-4 bg-black/20 rounded border border-white/10' />
                             </th>
                             <th className='py-3 px-4 text-sm font-semibold text-left'>Código</th>
                             <th className='py-3 px-4 text-sm font-semibold text-left'>Participantes</th>
                             <th className='py-3 px-4 text-sm font-semibold text-left'>Data de inscrição</th>
                             <th className='py-3 px-4 text-sm font-semibold text-left'>Data do check-in</th>
-                            <th className='py-3 px-4 text-sm font-semibold text-left'></th>
+                            <th style={{ width: 64 }} className='py-3 px-4 text-sm font-semibold text-left'></th>
                         </tr>
                     </thead>
 
                     <tbody>
                         {Array.from({ length: 8 }).map(() => {
                             return(
-                                <tr className='border-b border-white/10'>
-                                    <td className='py-3 px-4 text-sm text-zinc-300'><input type="checkbox" /></td>
+                                <tr className='border-b border-white/10 hover:bg-white/5'>
+                                    <td className='py-3 px-4 text-sm text-zinc-300'>
+                                        <input type="checkbox" className='size-4 bg-black/20 rounded border border-white/10 accent-orange-400' />
+                                    </td>
                                     <td className='py-3 px-4 text-sm text-zinc-300'>12312</td>
                                     <td className='py-3 px-4 text-sm text-zinc-300'>
                                         <div className='flex flex-col gap-1'>
@@ -40,7 +42,11 @@ export function AttendeeList() {
                                     </td>
                                     <td className='py-3 px-4 text-sm text-zinc-300'>7 dias atrás</td>
                                     <td className='py-3 px-4 text-sm text-zinc-300'>7 dias atrás</td>
-                                    <td className='py-3 px-4 text-sm text-zinc-300'></td>
+                                    <td className='py-3 px-4 text-sm text-zinc-300'>
+                                        <button className='bg-black/20 border border-white/10 rounded-md p-1.5'>
+                                            <MoreHorizontal className='size-4'/>
+                                        </button>
+                                    </td>
                                 </tr>
                             )
                         })} {/* METODO CRIADO PARA CRIAR UMA LISTAGEM COM 8 LINHAS DA TABELA PREENCHIDAS A FIM DE TESTES */}
@@ -52,7 +58,23 @@ export function AttendeeList() {
                                 Mostrando 10 de 228 itens
                             </td>
                             <td className='py-3 px-4 text-sm text-zinc-300 text-right' colSpan={3}>
-                                Página 1 de 23
+                                <div className='inline-flex items-center gap-8'>
+                                    <span>Página 1 de 23</span>
+                                    <div className='flex gap-1.5'>
+                                        <button className='bg-white/10 border border-white/10 rounded-md p-1.5'>
+                                            <ChevronsLeft className='size-4'/>
+                                        </button>
+                                        <button className='bg-white/10 border border-white/10 rounded-md p-1.5'>
+                                            <ChevronLeft className='size-4'/>
+                                        </button>
+                                        <button className='bg-white/10 border border-white/10 rounded-md p-1.5'>
+                                            <ChevronRight className='size-4'/>
+                                        </button>
+                                        <button className='bg-white/10 border border-white/10 rounded-md p-1.5'>
+                                            <ChevronsRight className='size-4'/>
+                                        </button>
+                                    </div>
+                                </div>
                             </td>
                         </tr>
                     </tfoot>

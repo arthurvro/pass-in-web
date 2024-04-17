b2940a862affe8b7ded3d7577c0f08d5d94694ac
Arthur VR <arthurolov@gmail.com>
2024-04-17 01:42:01 -0300

    feat: table componentizado

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -1,5 +1,6 @@
 import { ChevronLeft, ChevronRight, ChevronsLeft, ChevronsRight, MoreHorizontal, Search } from 'lucide-react'
 import { IconButton } from './icon-button'
+import { Table } from './table/table'
 
 export function AttendeeList() {
     return (
@@ -12,78 +13,76 @@ export function AttendeeList() {
                 </div>
             </div>
 
-            <div className='border border-white/10 rounded-lg'>
-                <table className='w-full'>
-                    <thead>
-                        <tr className='border-b border-white/1'>
-                            <th style={{ width: 48 }} className='py-3 px-4 text-sm font-semibold text-left'>
-                                <input type="checkbox" className='size-4 bg-black/20 rounded border border-white/10' />
-                            </th>
-                            <th className='py-3 px-4 text-sm font-semibold text-left'>Código</th>
-                            <th className='py-3 px-4 text-sm font-semibold text-left'>Participantes</th>
-                            <th className='py-3 px-4 text-sm font-semibold text-left'>Data de inscrição</th>
-                            <th className='py-3 px-4 text-sm font-semibold text-left'>Data do check-in</th>
-                            <th style={{ width: 64 }} className='py-3 px-4 text-sm font-semibold text-left'></th>
-                        </tr>
-                    </thead>
+            <Table>
+                <thead>
+                    <tr className='border-b border-white/1'>
+                        <th style={{ width: 48 }} className='py-3 px-4 text-sm font-semibold text-left'>
+                            <input type="checkbox" className='size-4 bg-black/20 rounded border border-white/10' />
+                        </th>
+                        <th className='py-3 px-4 text-sm font-semibold text-left'>Código</th>
+                        <th className='py-3 px-4 text-sm font-semibold text-left'>Participantes</th>
+                        <th className='py-3 px-4 text-sm font-semibold text-left'>Data de inscrição</th>
+                        <th className='py-3 px-4 text-sm font-semibold text-left'>Data do check-in</th>
+                        <th style={{ width: 64 }} className='py-3 px-4 text-sm font-semibold text-left'></th>
+                    </tr>
+                </thead>
 
-                    <tbody>
-                        {Array.from({ length: 8 }).map(() => {
-                            return(
-                                <tr className='border-b border-white/10 hover:bg-white/5'>
-                                    <td className='py-3 px-4 text-sm text-zinc-300'>
-                                        <input type="checkbox" className='size-4 bg-black/20 rounded border border-white/10 accent-orange-400' />
-                                    </td>
-                                    <td className='py-3 px-4 text-sm text-zinc-300'>12312</td>
-                                    <td className='py-3 px-4 text-sm text-zinc-300'>
-                                        <div className='flex flex-col gap-1'>
-                                            <span className='font-semibold text-white'>Arthur Vieira Ribeiro</span>
-                                            <span>arthurvieira@gmail.com</span>
-                                        </div>
-                                    </td>
-                                    <td className='py-3 px-4 text-sm text-zinc-300'>7 dias atrás</td>
-                                    <td className='py-3 px-4 text-sm text-zinc-300'>7 dias atrás</td>
-                                    <td className='py-3 px-4 text-sm text-zinc-300'>
-                                        {/* <button className='bg-black/20 border border-white/10 rounded-md p-1.5'>
-                                            <MoreHorizontal className='size-4'/>
-                                        </button> */}
-                                        <IconButton transparent> {/* criando condicional para aplicar o fundo transparente no botao */}
-                                            <MoreHorizontal className='size-4'/>
-                                        </IconButton>
-                                    </td>
-                                </tr>
-                            )
-                        })} {/* METODO CRIADO PARA CRIAR UMA LISTAGEM COM 8 LINHAS DA TABELA PREENCHIDAS A FIM DE TESTES */}
-                    </tbody>
-                    
-                    <tfoot>
-                        <tr>
-                            <td className='py-3 px-4 text-sm text-zinc-300' colSpan={3}>
-                                Mostrando 10 de 228 itens
-                            </td>
-                            <td className='py-3 px-4 text-sm text-zinc-300 text-right' colSpan={3}>
-                                <div className='inline-flex items-center gap-8'>
-                                    <span>Página 1 de 23</span>
-                                    <div className='flex gap-1.5'>
-                                        <IconButton>
-                                            <ChevronsLeft className='size-4'/>
-                                        </IconButton>
-                                        <IconButton>
-                                            <ChevronLeft className='size-4'/>
-                                        </IconButton>
-                                        <IconButton>
-                                            <ChevronRight className='size-4'/>
-                                        </IconButton>
-                                        <IconButton>
-                                            <ChevronsRight className='size-4'/>
-                                        </IconButton>
+                <tbody>
+                    {Array.from({ length: 8 }).map(() => {
+                        return(
+                            <tr className='border-b border-white/10 hover:bg-white/5'>
+                                <td className='py-3 px-4 text-sm text-zinc-300'>
+                                    <input type="checkbox" className='size-4 bg-black/20 rounded border border-white/10 accent-orange-400' />
+                                </td>
+                                <td className='py-3 px-4 text-sm text-zinc-300'>12312</td>
+                                <td className='py-3 px-4 text-sm text-zinc-300'>
+                                    <div className='flex flex-col gap-1'>
+                                        <span className='font-semibold text-white'>Arthur Vieira Ribeiro</span>
+                                        <span>arthurvieira@gmail.com</span>
                                     </div>
+                                </td>
+                                <td className='py-3 px-4 text-sm text-zinc-300'>7 dias atrás</td>
+                                <td className='py-3 px-4 text-sm text-zinc-300'>7 dias atrás</td>
+                                <td className='py-3 px-4 text-sm text-zinc-300'>
+                                    {/* <button className='bg-black/20 border border-white/10 rounded-md p-1.5'>
+                                        <MoreHorizontal className='size-4'/>
+                                    </button> */}
+                                    <IconButton transparent> {/* criando condicional para aplicar o fundo transparente no botao */}
+                                        <MoreHorizontal className='size-4'/>
+                                    </IconButton>
+                                </td>
+                            </tr>
+                        )
+                    })} {/* METODO CRIADO PARA CRIAR UMA LISTAGEM COM 8 LINHAS DA TABELA PREENCHIDAS A FIM DE TESTES */}
+                </tbody>
+                    
+                <tfoot>
+                    <tr>
+                        <td className='py-3 px-4 text-sm text-zinc-300' colSpan={3}>
+                            Mostrando 10 de 228 itens
+                        </td>
+                        <td className='py-3 px-4 text-sm text-zinc-300 text-right' colSpan={3}>
+                            <div className='inline-flex items-center gap-8'>
+                                <span>Página 1 de 23</span>
+                                <div className='flex gap-1.5'>
+                                    <IconButton>
+                                        <ChevronsLeft className='size-4'/>
+                                    </IconButton>
+                                    <IconButton>
+                                        <ChevronLeft className='size-4'/>
+                                    </IconButton>
+                                    <IconButton>
+                                        <ChevronRight className='size-4'/>
+                                    </IconButton>
+                                    <IconButton>
+                                        <ChevronsRight className='size-4'/>
+                                    </IconButton>
                                 </div>
-                            </td>
-                        </tr>
-                    </tfoot>
-                </table>
-            </div>
+                            </div>
+                        </td>
+                    </tr>
+                </tfoot>
+            </Table>
         </div>
     )
 
---
+++ src/components/table/table.tsx
@@ -0,0 +1,16 @@
+import { ComponentProps } from "react";
+
+
+/*
+Table() => Componente que engloba todos os elementos da tabela
+
+*/
+interface TableProps extends ComponentProps<'table'> {}
+
+export function Table(props: TableProps) {
+    return (
+        <div className='border border-white/10 rounded-lg'>
+            <table className='w-full' {...props}/> {/* Enviando props como children */}
+        </div>
+    )
+}
